ed58b487fd0cbcaf83a5e137989723e0fb6cae1c
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-09-20 20:20:42 +0500

    Task5

---
+++ Tyuiu.StoletovNA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+using Tyuiu.StoletovNA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.StoletovNA.Sprint0.Task5.V0.Test {
+    public class DataServiceTest {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [Test]
+        public void CheckSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [Test]
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [Test]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
